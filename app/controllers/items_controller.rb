1b190252d2becca3f82982e027396075f7ee1949
morinotakuman <okadatakuma825@icloud.com>
2020-09-28 18:46:13 +0900

    ActiveStrageの実装と仕上げ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.create(item_params)
+    if @item.valid?
+      redirect_to action: :index
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :categories_id, :state_id, :bearer_id, :area_id, :days_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
   belongs_to_active_hash :category
   belongs_to_active_hash :state
   belongs_to_active_hash :bearer
