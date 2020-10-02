9af11436b92f489c189a2689109f759cc2afc86a
morinotakuman <okadatakuma825@icloud.com>
2020-10-02 18:21:07 +0900

    レビューを参考に修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -39,9 +39,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to action: :index
+    if @item.destroy
+      redirect_to action: :index
+    else
+      render 'show'
+    end
   end
 
   private
