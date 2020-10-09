1c4d052241fef9b741a45da8e7175752f57fac18
morinotakuman <okadatakuma825@icloud.com>
2020-10-09 19:55:24 +0900

    レビューを参考に修正しました

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,12 @@
 class OrdersController < ApplicationController
-  before_action :redirect, only: [:index]
+  before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index]
 
   def index
     @order_address = OrderAddress.new
+    if user_signed_in? && current_user.id == @item.user_id || @item.order
+      redirect_to root_path
+    end
   end
 
   def create
@@ -22,26 +26,16 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:zip_code, :prefecture, :city, :street_number, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def pay_item
-    item = Item.find(params[:item_id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
-        amount: item.price,
+        amount: @item.price,
         card: address_params[:token],
         currency: 'jpy'
       )
-  end
-
-  def redirect
-    item = Item.find(params[:item_id])
-    if user_signed_in? && current_user.id == item.user_id
-      redirect_to root_path
-    end
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
-    if Order.find_by(item_id: params[:item_id])
-      redirect_to root_path
-    end
   end
 end
